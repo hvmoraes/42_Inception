be50c7addbb66747f34565db10c4fe9d1f5b8bf8
Henrique Moraes <kikovillamoraes@gmail.com>
2024-01-27 11:24:25 +0000

    Some changes made

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -4,11 +4,11 @@ mysql_install_db
 
 /etc/init.d/mysql start
 
-#Check if the database exists
+# Check if the database exists
 
 if [ -d "/var/lib/mysql/$DATABASE" ]
 then 
-	echo "Database already exists"
+	echo "Error! Database already exists"
 else
 
 # Set root option so that connexion without root password is not possible
@@ -24,12 +24,12 @@ Y
 Y
 _EOF_
 
-#Add a root user on 127.0.0.1 to allow remote connexion 
-#Flush privileges allow to your sql tables to be updated automatically when you modify it
-#mysql -uroot launch mysql command line client
+# Add a root user on 127.0.0.1 to allow remote connexion 
+# Flush privileges allow to your sql tables to be updated automatically when you modify it
+# mysql -uroot launch mysql command line client
 echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
 
-#Create database and user in the database for wordpress
+# Create database and user in the database for wordpress
 
 echo "CREATE DATABASE IF NOT EXISTS $DATABASE; GRANT ALL ON $DATABASE.* TO '$USER'@'%' IDENTIFIED BY '$PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
 fi
--- srcs/requirements/wordpress/tools/wp_conf.sh
+++ srcs/requirements/wordpress/tools/wp_conf.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-#check if wp-config.php exist
+# Check if wp-config.php exist
 if [ -f ./wp-config.php ]
 then
-	echo "wordpress already downloaded"
+	echo "Error! Wordpress already downloaded"
 else
 
-#Download wordpress and all config file
+# Download wordpress
 wget http://wordpress.org/latest.tar.gz
 tar xfz latest.tar.gz
 mv wordpress/* .
 rm -rf latest.tar.gz
 rm -rf wordpress
 
-#Inport env variables in the config file
+# Import env variables in the .env file
 sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
 sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
 sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
