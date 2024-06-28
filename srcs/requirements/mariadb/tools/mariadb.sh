82a33c3ef3946483fa0f5093792571a8a597d546
hcorrea- <henriquecvcmoraes@gmail.com>
2024-06-28 11:57:56 +0100

    .

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -2,24 +2,17 @@
 
 set -e
 
-mkdir -p /run/mysqld
-chown -R mysql:mysql /run/mysqld
-chmod 755 /run/mysqld
-
 service mariadb start
 
 sleep 5
 
+# Grant privileges and create database and user
 echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot -p"$MYSQL_ROOT_PASSWORD"
-
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" | mysql -uroot -p"$MYSQL_ROOT_PASSWORD"
-
-echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql -uroot -p"$MYSQL_ROOT_PASSWORD"
-echo "CREATE USER '$MYSQL_ADMIN'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql -uroot -p"$MYSQL_ROOT_PASSWORD"
-
-echo "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%'; FLUSH PRIVILEGES;" | mysql -uroot -p"$MYSQL_ROOT_PASSWORD"
-echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_ADMIN'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;" | mysql -uroot -p"$MYSQL_ROOT_PASSWORD"
+echo "CREATE DATABASE IF NOT EXISTS $WP_DB_NAME;" | mysql -uroot -p"$MYSQL_ROOT_PASSWORD"
+echo "CREATE USER '$WP_DB_USER'@'%' IDENTIFIED BY '$WP_DB_PASS';" | mysql -uroot -p"$MYSQL_ROOT_PASSWORD"
+echo "GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO '$WP_DB_USER'@'%'; FLUSH PRIVILEGES;" | mysql -uroot -p"$MYSQL_ROOT_PASSWORD"
 
 service mariadb stop
 
+# Execute command passed to the script (e.g., starting another process)
 exec "$@"
--- srcs/requirements/wordpress/tools/wp_conf.sh
+++ srcs/requirements/wordpress/tools/wp_conf.sh
@@ -8,16 +8,28 @@ else
     mv wordpress/* .
     rm -rf latest.tar.gz wordpress
 
-    sed -e "s/database_name_here/$MYSQL_DATABASE/g" \
-        -e "s/username_here/$MYSQL_USER/g" \
-        -e "s/password_here/$MYSQL_PASSWORD/g" \
+    sed -e "s/database_name_here/$WP_DB_NAME/g" \
+        -e "s/username_here/$WP_DB_USER/g" \
+        -e "s/password_here/$WP_DB_PASS/g" \
         -e "s/localhost/$MYSQL_HOSTNAME/g" \
         wp-config-sample.php > wp-config.php
 
     chown www-data:www-data wp-config.php
     chmod 640 wp-config.php
 
-    echo "WordPress downloaded and configured."
+    echo "WordPress downloaded and database connected."
+
+    if [ ! -f "/var/www/html/index.html" ]; then
+        wp core download
+        wp config create --dbname=$WP_DB_NAME --dbuser=$WP_DB_USER --dbpass=$WP_DB_PASS --dbhost=$MYSQL_HOSTNAME --dbcharset="utf8" --dbcollate="utf8_general_ci"
+        wp core install --url=$DOMAIN/wordpress --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_MAIL
+        wp user create $WP_USER $WP_MAIL --role=author --user_pass=$WP_PASS
+        wp theme install inspiro --activate
+        wp plugin update --all 
+
+        echo "WordPress configured."
+    fi
 fi
 
-/usr/sbin/php-fpm7.4 -F 
+# Start PHP-FPM
+/usr/sbin/php-fpm7.4 -F
