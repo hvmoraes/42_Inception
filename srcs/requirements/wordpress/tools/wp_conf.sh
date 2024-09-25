f2786bbeb171538247adce69a72370e113640aec
hcorrea- <henriquecvcmoraes@gmail.com>
2024-09-25 11:55:58 +0100

    .

--- srcs/requirements/wordpress/tools/wp_conf.sh
+++ srcs/requirements/wordpress/tools/wp_conf.sh
@@ -29,7 +29,7 @@ else
     if [ ! -f "/var/www/html/index.html" ]; then
         wp --allow-root core download
         wp --allow-root config create --dbname=$WP_DB_NAME --dbuser=$WP_DB_USER --dbpass=$WP_DB_PASS --dbhost=$MYSQL_HOSTNAME --dbcharset="utf8" --dbcollate="utf8_general_ci"
-        wp --allow-root core install --url=$DOMAIN/wordpress --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_MAIL
+        wp --allow-root core install --url=$DOMAIN --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_MAIL
         wp --allow-root user create $WP_USER $WP_MAIL --role=author --user_pass=$WP_PASS
         #wp --allow-root theme install inspiro --activate
         #wp --allow-root plugin update --all 
